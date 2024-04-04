fc173d191255ee4e29c054feca51c51aad95aa7d
Thien Le Ba <thienlb@tech.est-rouge.com>
2024-04-04 11:59:08 +0700

    Adding web-spider version 2

---
+++ 01-asynchronous-control-flow-pattern-with-callbacks/04-web-spider-v2/utils.js
@@ -0,0 +1,44 @@
+import path from "path";
+import { URL } from "url";
+import slug from "slug";
+import cheerio from "cheerio";
+import exp from "constants";
+
+function getLinkUrl(currentUrl, element) {
+  const parsedLink = new URL(element.attribs.href || "", currentUrl);
+  const currentParseUrl = new URL(currentUrl);
+  if (
+    parsedLink.hostname !== currentParseUrl.hostname ||
+    !parsedLink.pathname
+  ) {
+    return null;
+  }
+  return parsedLink.toString();
+}
+
+export function urlToFilename(url) {
+  const parsedUrl = new URL(url);
+  const urlPath = parsedUrl.pathname
+    .split("/")
+    .filter(function (component) {
+      return component !== "";
+    })
+    .map(function (component) {
+      return slug(component, { remove: null });
+    })
+    .join("/");
+  let filename = path.join(parsedUrl.hostname, urlPath);
+  if (!path.extname(filename).match(/htm/)) {
+    filename += ".html";
+  }
+
+  return filename;
+}
+
+export function getPageLinks(currentUrl, body) {
+  return Array.from(cheerio.load(body)("a"))
+    .map(function (element) {
+      return getLinkUrl(currentUrl, element);
+    })
+    .filter(Boolean);
+}
